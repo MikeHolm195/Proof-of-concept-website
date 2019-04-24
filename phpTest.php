b4bf69fef1b210ac2ccb921ef3d0af7f674625d0
MikeHolm195 <49945254+MikeHolm195@users.noreply.github.com>
2019-04-24 16:49:43 +0200

    Add files via upload

---
+++ phpTest.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang = "en-uk">
+	<head>
+		<meta name = "Author" content = "Website creators" charset = "UTF-8" />
+		<title>The Test Page</title>
+		<link rel = "icon" type = "image/jpg" href = "Images/favicon.jpg" />
+		<link rel = "stylesheet" href = "Style/mainStyle.css" />
+	</head>
+	<body>
+		<h1 class = "centerTitle">Welcome to the PHP Test Page</h1>
+		<a href = "index.html"><h3 class = "centerTitle">Homepage</h3></a>
+		<a href = "testPage.html"><h3 class = "centerTitle">The Test Page</h3></a>
+		<?php
+		echo "<p>Hello. This was made by the PHP script</p>";
+		?>
+	</body>
+</html>
